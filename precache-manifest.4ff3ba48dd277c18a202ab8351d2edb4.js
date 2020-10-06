71a9c4eb839d678707e5a23e7810472e865ce259
Pooja-Programmer <70345222+Pooja-Programmer@users.noreply.github.com>
2020-10-06 13:29:23 -0500

    Updates

---
+++ precache-manifest.4ff3ba48dd277c18a202ab8351d2edb4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1895839be22732d5e4efe115512e08ba",
+    "url": "/Cinemeya/index.html"
+  },
+  {
+    "revision": "6e05262c516531d4d4d7",
+    "url": "/Cinemeya/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "8710c14b9b298042fed3",
+    "url": "/Cinemeya/static/css/main.c8bd8fe3.chunk.css"
+  },
+  {
+    "revision": "6e05262c516531d4d4d7",
+    "url": "/Cinemeya/static/js/2.4eda7785.chunk.js"
+  },
+  {
+    "revision": "d81c6c91afd2bb4bf8caccddf0472235",
+    "url": "/Cinemeya/static/js/2.4eda7785.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8710c14b9b298042fed3",
+    "url": "/Cinemeya/static/js/main.5893ab7f.chunk.js"
+  },
+  {
+    "revision": "89a6580100b3aed08040",
+    "url": "/Cinemeya/static/js/runtime-main.a4d9a76c.js"
+  }
+]);
